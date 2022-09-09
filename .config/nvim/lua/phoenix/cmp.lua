3b58c85a16e00cf8caaab20b39dc4d362879e594
Lenny Critchley <LennyPhoenixC@gmail.com>
2022-09-09 15:33:36 +0100

    [nvim] Fix check_backspace
    Parentheses problems

--- .config/nvim/lua/phoenix/cmp.lua
+++ .config/nvim/lua/phoenix/cmp.lua
@@ -15,7 +15,7 @@ end
 require("luasnip/loaders/from_vscode").lazy_load()
 
 local check_backspace = function()
-    local col = vim.fn.col("." - 1)
+    local col = vim.fn.col(".") - 1
     return col == 0 or vim.fn.getline("."):sub(col, col):match("%s")
 end
 
