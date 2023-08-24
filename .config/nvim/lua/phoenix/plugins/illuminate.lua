1203427c51bdc289c76cbd5dcdc4e416db29b9f6
Lenny Critchley <LennyPhoenixC@gmail.com>
2023-08-25 00:44:35 +0100

    [nvim] Add lazy lock and some plugins

---
+++ .config/nvim/lua/phoenix/plugins/illuminate.lua
@@ -0,0 +1,22 @@
+local function config()
+    vim.cmd([[
+        hi def IlluminatedWordText gui=underline
+        hi def IlluminatedWordRead gui=underline
+        hi def IlluminatedWordWrite gui=underline
+    ]])
+
+    require("illuminate").configure {
+        providers = {
+            "lsp",
+            "treesitter",
+            "regex",
+        },
+        delay = 0,
+        under_cursor = true,
+    }
+end
+
+return {
+    "RRethy/vim-illuminate",
+    config = config
+}
