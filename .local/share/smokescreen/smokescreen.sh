a7443046994ed0ac09f5426dbee42066022e8001
Lenny Critchley <LennyPhoenixC@gmail.com>
2023-09-19 23:11:39 +0100

    [smokescreen] Implement smokescreen

---
+++ .local/share/smokescreen/smokescreen.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env sh
+
+# Depends on:
+# - scrot
+# - bc
+# - imagemagick
+# - i3lock
+
+# Settings
+output="/tmp/lockimgbg"
+scale_factor=8
+blur="8x4"
+brightness_threshold=33000
+light_icon="$HOME/.local/share/smokescreen/circlelock.png"
+dark_icon="$HOME/.local/share/smokescreen/circlelockdark.png"
+
+down_factor=$(echo "scale=2;100/$scale_factor" | bc)
+up_factor=$(echo "scale=2;100*$scale_factor" | bc)
+
+scrot --format bmp -f - | convert - -scale "$down_factor%" -blur "$blur" -sample "$up_factor%" "png:$output"
+
+width=$(convert "$light_icon" -format "%w" info:)
+height=$(convert "$light_icon" -format "%h" info:)
+brightness=$(convert "$output" -gravity center -crop "${width}x${height}+0+0" +repage -colorspace Gray -format "%[mean]" info:)
+
+if (( $(echo "$brightness > $brightness_threshold" | bc -l) ))
+then
+    icon=$dark_icon
+else
+    icon=$light_icon
+fi
+
+convert "$output" "$icon" -gravity center -composite "$output"
+
+i3lock -i "$output"
