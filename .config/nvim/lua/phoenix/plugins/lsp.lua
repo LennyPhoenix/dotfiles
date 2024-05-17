35e8d4384d0a56ab79f9f4edc38e960b8ada76fc
Lenny Critchley <LennyPhoenixC@gmail.com>
2024-05-17 20:51:24 +0100

    [nvim] fix diagnostics

--- .config/nvim/lua/phoenix/plugins/lsp.lua
+++ .config/nvim/lua/phoenix/plugins/lsp.lua
@@ -1,19 +1,25 @@
 local function setup()
-    local signs = {
-        { name = "DiagnosticSignError", text = "" },
-        { name = "DiagnosticSignWarn", text = "" },
-        { name = "DiagnosticSignHint", text = "" },
-        { name = "DiagnosticSignInfo", text = "" },
-    }
-
-    for _, sign in ipairs(signs) do
-        vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
-    end
-
     local config = {
         virtual_text = true,
         signs = {
-            active = signs,
+            text = {
+                [vim.diagnostic.severity.ERROR] = "",
+                [vim.diagnostic.severity.WARN] = "",
+                [vim.diagnostic.severity.HINT] = "",
+                [vim.diagnostic.severity.INFO] = "",
+            },
+            linehl = {
+                [vim.diagnostic.severity.ERROR] = "DiagnosticError",
+                [vim.diagnostic.severity.WARN] = "DiagnosticWarn",
+                [vim.diagnostic.severity.HINT] = "DiagnosticHint",
+                [vim.diagnostic.severity.INFO] = "DiagnosticInfo",
+            },
+            numhl = {
+                [vim.diagnostic.severity.ERROR] = "DiagnosticError",
+                [vim.diagnostic.severity.WARN] = "DiagnosticWarn",
+                [vim.diagnostic.severity.HINT] = "DiagnosticHint",
+                [vim.diagnostic.severity.INFO] = "DiagnosticInfo",
+            },
         },
         update_in_insert = true,
         underline = true,
