171c56dcd779c3fa2ec698881795f6051f5e7e37
Lenny Critchley <LennyPhoenixC@gmail.com>
2024-10-12 17:30:07 +0100

    [nvim] Disable treesitter on buffers with huge lines

--- .config/nvim/lua/phoenix/plugins/treesitter.lua
+++ .config/nvim/lua/phoenix/plugins/treesitter.lua
@@ -3,7 +3,22 @@ local function config()
 		auto_install = true,
 		highlight = {
 			enable = true,
-			disable = {},
+			disable = function(lang, bufnr)
+				-- Disable for files with more than 4k columns
+
+				-- Get all lines in the buffer
+				local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
+
+				-- Iterate through each line and get the column count
+				for _, line in ipairs(lines) do
+					local col_count = vim.fn.strdisplaywidth(line)
+					if col_count > vim.bo[bufnr].synmaxcol then
+						return true
+					end
+				end
+
+				return false
+			end,
 		},
 		autopairs = {
 			enable = true,
